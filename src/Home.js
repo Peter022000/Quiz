df8667af24209650554d88dca6f139e646700ab1
Piotr Duda <piotr.duda00@gmail.com>
2022-12-18 20:43:20 +0100

    fix

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import {View, Text, TouchableOpacity,StyleSheet, Linking, ScrollView, Button} from 'react-native';
 
 const Home = (props) => {
